ebf4986a11017ac4360421ee89f36c7fcc3dcd49
Alex Loukissas <aloukissas@gmail.com>
2020-10-09 18:22:10 -0700

    Use env var

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { loadClerk, ClerkProvider } from "@clerk/clerk-react";
 import { NavBar } from "@components";
 import "../styles/globals.css";
 
-loadClerk("auth.usoaw.47qim.lcl.dev");
+loadClerk(process.env.NEXT_PUBLIC_CLERK_URL || "");
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <ClerkProvider>
-      {router.pathname.startsWith("/dashboard") && <NavBar />}
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+      </Head>
+      <ClerkProvider>
+        {router.pathname.startsWith("/dashboard") && <NavBar />}
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </>
   );
 }
